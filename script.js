4cd35c33a0e1f9d0acc55f6d80beccea291aec1d
srishti <srishtimanna2@gmail.com>
2025-01-26 23:10:02 +0530

    .

--- script.js
+++ script.js
@@ -24,3 +24,7 @@
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
 console.log(inputYear + 18);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
