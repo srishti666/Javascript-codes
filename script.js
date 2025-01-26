73e9a9649e83ea7968cb34242768c1350f51673e
srishti <srishtimanna2@gmail.com>
2025-01-26 20:21:07 +0530

    type casting

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const massMark= 78;
-const heightMark= 1.69;
-const massJohn= 92;
-const heightJohn= 1.95;
+// const massMark= 78;
+// const heightMark= 1.69;
+// const massJohn= 92;
+// const heightJohn= 1.95;
 
-const BMIMark = massMark/(heightMark*heightMark);
-const BMIJohn = massJohn/(heightJohn*heightJohn);
+// const BMIMark = massMark/(heightMark*heightMark);
+// const BMIJohn = massJohn/(heightJohn*heightJohn);
 
-const markHigherBMI = BMIMark > BMIJohn
-console.log(BMIMark,BMIJohn, markHigherBMI);
+// const markHigherBMI = BMIMark > BMIJohn
+// console.log(BMIMark,BMIJohn, markHigherBMI);
 
-console.log(BMIMark)
+// console.log(BMIMark)
+
+// console.log(`String with \n\
+//     multiple \n\
+//     lines`);
+
+// console.log(`String
+//     multiple
+//     lines`);
+
+//typecasting
+
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 18);
---
+++ typeConversion.js
@@ -0,0 +1,3 @@
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 18);
