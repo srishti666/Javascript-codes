a6f9edf9ee9d9eca7c2ad28c82e5c4bb0e1b45ed
srishti <srishtimanna2@gmail.com>
2025-01-27 22:35:02 +0530

    true false

---
+++ falsyValues.js
@@ -0,0 +1,4 @@
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Srishti'));
+console.log(Boolean({}));
--- script.js
+++ script.js
@@ -28,3 +28,6 @@ console.log(Number(inputYear) + 18);
 
 console.log(Number('Jonas'));
 console.log(typeof NaN);
+
+console.log('I am ' + 23 + ' years old');
+
