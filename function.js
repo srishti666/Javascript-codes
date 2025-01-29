eb64e7aa934be3d1b005e8db1cbfca842609ce76
srishti <srishtimanna6@gmail.com>
2025-01-30 00:00:15 +0530

    correction in function

--- function.js
+++ function.js
@@ -8,7 +8,7 @@ logger();
 
 function favouriteProcessor(apples, oranges){
     console.log(apples, oranges)
-    const juice = `Juice with $(apples) apples and $(oranges) oranges`;
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
     return juice;
 }
 favouriteProcessor(5,8);
