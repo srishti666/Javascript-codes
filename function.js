4ca058573048538bbc03629287871b40e99f0485
srishti <srishtimanna6@gmail.com>
2025-01-31 08:54:38 +0530

    function declaration vs expresssion

--- function.js
+++ function.js
@@ -15,6 +15,26 @@ function fruitProcessor(apples, oranges){
 const appleJuice = fruitProcessor(5,0);
 console.log(appleJuice);
 
-
 const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
+
+//parameters are placeholders in a function and arguments are the actual values we use to fill in the placeholder
+//function declaration vs exepressions
+
+//type 1
+//Function declaration
+function calcAge1(birthyear){
+    return 2037 - birthyear;
+}
+
+const age1 = calcAge1(1991);
+
+//type 2
+//Function expression
+const calcAge2 = function (birthyear){
+    return 2037 - birthyear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
