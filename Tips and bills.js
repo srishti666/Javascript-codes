4d6d40e456281dcd382da9d4b9c19148cb90aad0
srishti <srishtimanna6@gmail.com>
2025-02-04 09:17:07 +0530

    bills and tips

---
+++ Tips and bills.js
@@ -0,0 +1,17 @@
+// const calcTip = [a, b, c] => [a*15/100, b*15/100, c*15/100];
+// console.log(calcTip)
+
+// // const bills = [125, 555, 44];
+
+// const tips = calcTip(125, 555, 44)
+// console.log(tips);
+
+const calcTip = function(bill){
+    return bill  >= 50 && bill <=300 ? bill * 0.15 : bill * 0.2
+}
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills[2])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips);
