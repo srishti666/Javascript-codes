68dfc4bab45830f196f7d02f95452271b63998d3
srishti <srishtimanna6@gmail.com>
2025-02-06 00:00:15 +0530

    object cont

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+//in objects the order of the value is not maintained when we want to retrieve them
 const jonasArray = [
     'Jonas',
     'Schmedtann',
@@ -5,3 +6,21 @@ const jonasArray = [
     'teacher',
     ['Mike', 'Bob', 'Lucy']
 ];
+
+const jonas = {
+    firstName: 'Srishti',
+    lastName: 'Manna',
+    age: 2025 - 2003,
+    job: 'SDE',
+    friends: ['Astha', 'lily', 'aden']
+};
+
+console.log(jonas);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['first' + nameKey]);
+
+const interestedIn = prompt['What do you want to know about JOnas'];
+console.log(interestedIn);
