5b3fb8acd88a0ce2f073ee52ba22a10ba1259d3f
srishti <srishtimanna6@gmail.com>
2025-02-06 08:15:58 +0530

    dot vs bracket notation

--- objects.js
+++ objects.js
@@ -22,5 +22,16 @@ const nameKey = 'Name';
 console.log(jonas['first' + nameKey]);
 console.log(jonas['first' + nameKey]);
 
-const interestedIn = prompt['What do you want to know about JOnas'];
-console.log(interestedIn);
+const interestedIn = prompt('What do you want to know about Srishti? Choose between firstName, lastName , age , job and friends');
+
+if(jonas[interestedIn]){
+console.log(jonas[interestedIn]);
+}
+else {
+    console.log("this value doesnt exist");
+}
+
+
+//Dot notation : Preferred when the property name is a valid identifier (alphanumeric, underscores, and dollar signs, but cannot start with a number).
+
+//Bracket Notation: Necessary when the property name is not a valid identifier, is a variable, or contains special characters.
