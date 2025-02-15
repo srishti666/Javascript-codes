dc9869b7d1337138210c51ce6ec89b43df9b35b1
srishti <srishtimanna6@gmail.com>
2025-02-15 23:28:58 +0530

    .

--- modal window/script.js
+++ modal window/script.js
@@ -4,4 +4,6 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelector('.open-modal');
+const btnOpenModal = document.querySelector('.open-modal');
 console.log(btnOpenModal);
+
