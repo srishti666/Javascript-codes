3176424e85ff2e4d51755138bcf18c8b7abbe22c
srishti <srishtimanna6@gmail.com>
2025-02-16 18:43:55 +0530

    handling esc keyword

--- modal window/script.js
+++ modal window/script.js
@@ -6,15 +6,24 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
 for(let i = 0; i<btnOpenModal.length; i++)
     btnOpenModal[i].addEventListener
-        ('click', function() {
-            console.log('Button clicked');
-            modal.classList.remove('hidden');
-            overlay.classList.remove('hidden');
-    });
+        ('click', openModal);
 
-    btnCloseModal.addEventListener('click', function() {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    })
+  
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    console.log('A key was pressed');
+});
