f9651aafc561d38658841a2ca802cc8a84248fb8
srishti <srishtimanna6@gmail.com>
2025-02-16 16:58:48 +0530

    modal window

--- modal window/script.js
+++ modal window/script.js
@@ -3,7 +3,18 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.open-modal');
-const btnOpenModal = document.querySelector('.open-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
+for(let i = 0; i<btnOpenModal.length; i++)
+    btnOpenModal[i].addEventListener
+        ('click', function() {
+            console.log('Button clicked');
+            modal.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+    });
+
+    btnCloseModal.addEventListener('click', function() {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    })
