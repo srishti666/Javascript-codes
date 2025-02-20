a8fc9afb64a948acf428249f593d94daa12e2521
srishti <srishtimanna6@gmail.com>
2025-02-20 10:55:39 +0530

    slice n splice

--- arrays.js
+++ arrays.js
@@ -19,24 +19,43 @@
 //unshift and push methods
 //unshift-> add elements to the beginning of the array
 //push -> add elemenst to the end of the array
-const friends = ['Michael', 'Robin', 'Steven'];
+// const friends = ['Michael', 'Robin', 'Steven'];
 
-const newLength = friends.push('Normi');
-console.log(friends);
-console.log(newLength);
+// const newLength = friends.push('Normi');
+// console.log(friends);
+// console.log(newLength);
 
-friends.unshift('John');
-console.log(friends);
+// friends.unshift('John');
+// console.log(friends);
 
-//remove elements
-friends.pop();
-console.log(friends);
-//remove first element
-friends.shift();
-console.log(friends);
+// //remove elements
+// friends.pop();
+// console.log(friends);
+// //remove first element
+// friends.shift();
+// console.log(friends);
 
-console.log(friends.indexOf('Steven'));
-//boolean: present or not
+// console.log(friends.indexOf('Steven'));
+// //boolean: present or not
 
-console.log(friends.includes('Steven'));  //true
-console.log(friends.includes('Bob'));  //false
+// console.log(friends.includes('Steven'));  //true
+// console.log(friends.includes('Bob'));  //false
+
+//SLICE
+let arr = ['a', 'b', 'c','d','e'];
+
+console.log(arr.slice(2));  //2 to end ; c, d, e
+console.log(arr.slice(2, 4));  //from 2 to 4-1 i.e. c,d
+console.log(arr.slice(-1)); //last element
+console.log(arr.slice(-2)); //last two element
+console.log(arr.slice(1,-1)); // 1 to -1(e) + 1 ; b ,c, d
+
+//to create a shallow copy
+console.log([...arr]); 
+console.log((arr.slice())); 
+
+//SPLICE -> mutates the array
+console.log(arr.splice(2));
+
+
+
