6dafe83c974b1abe34d2c6176db06f3c9e3453df
srishti <srishtimanna6@gmail.com>
2025-02-28 10:05:12 +0530

    Modules

--- classes.js
+++ classes.js
@@ -1,28 +1,10 @@
+import { Teacher } from './teacher';
 
-class Person {
-    constructor(name){
-        this.name = name ; 
-    }
-    walk(){
-        console.log("Walk");
-    }
-}
-
-const person = new Person('Srishti');
-person.walk();
 
 
 //Inheritance
 
-class Teacher extends Person{
-    constructor(name, degree){
-        super(name);
-        this.degree = degree;
-    }
-    teach() {
-        console.log("teach");
-    }
-}
 
 const teacher = new Teacher("Srishti", "BTech");
+teacher.teach();
 
