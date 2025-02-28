7fb5d6b37745a3e39a78d7385a383a8050e5b2e1
srishti <srishtimanna6@gmail.com>
2025-02-28 11:52:59 +0530

    react module structure

--- classes.js
+++ classes.js
@@ -1,5 +1,6 @@
 import Teacher, { promote} from './teacher';
-
+import React, { Component } from 'react';
+// react is the default export from a module whereas {Component} is a named export
 
 
 //Default -> import ... from '';
