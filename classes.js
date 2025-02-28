b0e903d287c2881ebdf1ae7b1b33c378eea93e8e
srishti <srishtimanna6@gmail.com>
2025-02-28 09:06:27 +0530

    Inheritance

--- classes.js
+++ classes.js
@@ -6,7 +6,23 @@ class Person {
     walk(){
         console.log("Walk");
     }
-
 }
 
-const person = new Person()
+const person = new Person('Srishti');
+person.walk();
+
+
+//Inheritance
+
+class Teacher extends Person{
+    constructor(name, degree){
+        super(name);
+        this.degree = degree;
+    }
+    teach() {
+        console.log("teach");
+    }
+}
+
+const teacher = new Teacher("Srishti", "BTech");
+
