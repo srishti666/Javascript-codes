99d618c98c2c8c7970f223be4c293b9be52a734b
srishti <srishtimanna6@gmail.com>
2025-03-01 10:42:46 +0530

    ternary operator

---
+++ ReactForJS.js
@@ -0,0 +1,13 @@
+{/* <button onClick={() => {
+    console.log("Hello World");
+}}>
+
+</button> */}
+
+//ternary operator
+let age = 10;
+let name = age > 10 ? "swiss" : "Jack";
+
+let vote = age >18 ? "yes" :  " No";
+
+ 
