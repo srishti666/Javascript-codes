996c53b9a710ecb59b5fd5e8b1d4e3fd8f392f1d
srishti <srishtimanna6@gmail.com>
2025-03-04 08:36:47 +0530

    reduce function

--- map.js
+++ map.js
@@ -35,7 +35,29 @@ const arr = [ 5, 1, 3, 2, 6];
 function isEven(x){
     return x % 2 === 0;
 }
+function isGreater(x){
+    return x > 4;
+}
+//arrow func form
+const outp = arr.filter(function isGreater(x) {
+    return x > 4;
+});
+
 //when its going to 2, 6 its returning false so the func is returning the remaining odd numbers
 
-const outputt = arr.filter(isOdd);
+// const output2 = arr.filter((x) => x < 3);
+
+const outputt = arr.filter(isGreater);
 console.log(outputt);
+
+//Reduce function
+
+function findSum (arr) {
+    let sum = 0;
+    for (let i =0; i<arr.length; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+console.log(findSum(arr))
